4b7db1994f81107a84c1282c902dca0182b31c43
Sarandeep123 <102315666+Sarandeep123@users.noreply.github.com>
2022-03-28 20:59:49 +0530

    Add files via upload

---
+++ sum of n natural,even,odd.py
@@ -0,0 +1,7 @@
+n=int(input("Enter a number"))
+N=n*(n+1)//2
+E=n*(n+1)
+O=n*n
+print("The sum of first n natural numbers is:",N)
+print("The sum of first n even numbers is:",E)
+print("The sum of first n odd numbers is:",O)
